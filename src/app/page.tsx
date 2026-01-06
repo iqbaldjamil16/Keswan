f89af5baee46b27bb60376d2b4a6c4ac0be288a9
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-01-06 10:32:22 +0000

    perbesar sedikit ukuran huruf pelayanan kesehatan hewan saat tampil di h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/ca
 
 export default function Home() {
   return (
-    <div className="container px-4 sm:px-8 py-4 md:py-8">
+    <div className="container px-3 sm:px-8 py-4 md:py-8">
       <div className="max-w-4xl mx-auto">
         <Card>
           <CardHeader>
-            <CardTitle className="text-xl md:text-3xl font-bold tracking-tight font-headline">Pelayanan Kesehatan Hewan</CardTitle>
+            <CardTitle className="text-2xl md:text-3xl font-bold tracking-tight font-headline">Pelayanan Kesehatan Hewan</CardTitle>
             <CardDescription className="text-muted-foreground pt-2 text-sm md:text-base">
               Input detail pelayanan yang telah dilakukan
             </CardDescription>
