91e9119d7225f267f80d232e1378d503a091f375
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-01-06 10:30:45 +0000

    paskan kalimat judul pelayanan kesehatan hewan saat tampil di hp sejajar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="max-w-4xl mx-auto">
         <Card>
           <CardHeader>
-            <CardTitle className="text-2xl md:text-3xl font-bold tracking-tight font-headline">Pelayanan Kesehatan Hewan</CardTitle>
+            <CardTitle className="text-xl md:text-3xl font-bold tracking-tight font-headline">Pelayanan Kesehatan Hewan</CardTitle>
             <CardDescription className="text-muted-foreground pt-2 text-sm md:text-base">
               Input detail pelayanan yang telah dilakukan
             </CardDescription>
