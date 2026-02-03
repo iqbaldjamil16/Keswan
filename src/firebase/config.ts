49e9b556f6cade6830f6893de52dcc129655a3b1
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-02-03 08:24:17 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6201683056-7c65e",
-  "appId": "1:268652550452:web:263e459e98e10ccc246d61",
-  "apiKey": "AIzaSyBsLaTDhrWe0uN55L49n3JQa9gMiFQyV1k",
-  "authDomain": "studio-6201683056-7c65e.firebaseapp.com",
+  "projectId": "vaksinasi-11512062-b4648",
+  "appId": "1:184338637766:web:e2da10aa6d79d230bc08c7",
+  "apiKey": "AIzaSyBhMadEq1BJkb9h7BROd9twQghs1kF20tc",
+  "authDomain": "vaksinasi-11512062-b4648.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "268652550452"
+  "messagingSenderId": "184338637766"
 };
