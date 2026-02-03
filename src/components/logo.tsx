e1f3cd4b41bb2b6e8f23f26a129871dd2edc7a5d
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-02-03 09:58:00 +0000

    ubah warna text menjadi hitam (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center" aria-label="PKH Mateng Homepage">
-      <span className="text-xl font-bold text-foreground font-headline" suppressHydrationWarning>PKH Mateng</span>
+      <span className="text-xl font-bold text-black font-headline" suppressHydrationWarning>PKH Mateng</span>
     </Link>
   );
 }
